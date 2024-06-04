248541b63d1f19661869d2d49797b1c3ee6447ec
Raditya Rizky <91870047+raditya-rizky@users.noreply.github.com>
2024-06-04 12:22:34 +0800

    feat: tru using column

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,39 @@ import 'package:myapp/home.dart';
 
 void main() {
   runApp(
-    const MaterialApp(home: Home()),
+    const MaterialApp(home: Sandbox()),
   );
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: 100,
+            color: Colors.red,
+            child: const Text('one'),
+          ),
+          Container(
+            width: 200,
+            color: Colors.green,
+            child: const Text('two'),
+          ),
+          Container(
+            width: 300,
+            color: Colors.blue,
+            child: const Text('three'),
+          ),
+        ],
+      ),
+    );
+  }
 }
